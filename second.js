653f09121dab8ff3c02bd7b4e8ba4b04aa0d8d66
regijs <32583136+regijs@users.noreply.github.com>
2017-10-07 00:36:19 -0300

    Add files via upload

---
+++ second.js
@@ -0,0 +1,14 @@
+(function () {
+  var head = document.getElementsByTagName("head")[0], script;
+  script = document.createElement("script");
+  script.type = "text/x-mathjax-config";
+  script[(window.opera ? "innerHTML" : "text")] =
+    "MathJax.Hub.Config({\n" +
+    "  tex2jax: { inlineMath: [['$','$'], ['\\\\(','\\\\)']] }\n" +
+    "});";
+  head.appendChild(script);
+  script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_HTML";
+  head.appendChild(script);
+})();
